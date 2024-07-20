7aeb799ba803efaa1e93cfce97fdbb9ad4515993
MuhammadMagdy7 <mmmagdi645@gmail.com>
2024-07-20 23:19:59 +0300

    edit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,74 @@
+// import "./globals.css";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import { getServerSession } from "next-auth";
+// import SessionProvider from "@/utils/SessionProvider";
+// import Navbar from "@/components/Layout/Navbar";
+// import Footer from "@/components/Layout/Footer";
+// import Whatsapp from "@/components/Layout/Whatsapp";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Version AVI",
+//   description:
+//     "Versionavi is a premier company specializing in the rental and sales of high-quality audio-visual equipment in Egypt. We offer comprehensive services including LED screens, sound systems, lighting, and photography/videography. With over 13 years of experience and a dedicated team, we ensure innovative and reliable solutions for all your event needs. Join us today and experience the Versionavi difference.",
+//     icons: {
+//     icon: "/img/logo.ico",
+//   },
+//   };
+
+
+// export default async function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const session = await getServerSession();
+//   return (
+//     <html lang="en">
+//       <head>
+//       <link rel="icon" href="/img/logo.ico" sizes="any" />
+//       </head>
+//       <body className={inter.className}>
+//         <SessionProvider session={session}>
+//           <Navbar />
+//           <div className="min-h-screen bg-gradient-to-b from-white to-blue-50">
+
+//           {children}
+//           </div>
+
+//           <Footer />
+//           <Whatsapp />
+//         </SessionProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lora } from "next/font/google"; // استيراد الخط lora مع الأوزان المحددة
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/utils/SessionProvider";
 import Navbar from "@/components/Layout/Navbar";
 import Footer from "@/components/Layout/Footer";
 import Whatsapp from "@/components/Layout/Whatsapp";
 
-const inter = Inter({ subsets: ["latin"] });
+// تحديد الأوزان المطلوبة للخط Roboto
+const lora = Lora({
+  subsets: ["latin"],
+  weight: [ "600", "400", "500", "700"], // تحديد الأوزان هنا
+});
 
 export const metadata: Metadata = {
   title: "Version AVI",
   description:
     "Versionavi is a premier company specializing in the rental and sales of high-quality audio-visual equipment in Egypt. We offer comprehensive services including LED screens, sound systems, lighting, and photography/videography. With over 13 years of experience and a dedicated team, we ensure innovative and reliable solutions for all your event needs. Join us today and experience the Versionavi difference.",
-    icons: {
+  icons: {
     icon: "/img/logo.ico",
   },
-  };
-
+};
 
 export default async function RootLayout({
   children,
@@ -28,16 +79,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/img/logo.ico" sizes="any" />
+        <link rel="icon" href="/img/logo.ico" sizes="any" />
       </head>
-      <body className={inter.className}>
+      <body className={lora.className}> 
         <SessionProvider session={session}>
           <Navbar />
           <div className="min-h-screen bg-gradient-to-b from-white to-blue-50">
-
-          {children}
+            {children}
           </div>
-
           <Footer />
           <Whatsapp />
         </SessionProvider>
--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -2,12 +2,18 @@
 import { motion } from 'framer-motion';
 
 const HeaderSec = ({ title, color = "text-secondaryColor" }) => {
+  const gradientStyle = {
+    background: 'linear-gradient(to right, #69CCDF, #EA3478)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  };
   return (
     <motion.div
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className={`text-center ${color} font-extrabold py-5 mb-5 px-4 overflow-hidden`}
+      className={`text-center  bg-gradient-to-b from-secondaryColor to-primaryColor font-extrabold py-5 mb-5 px-4 overflow-hidden`}
+      style={gradientStyle}
     >
       <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
         {title}
